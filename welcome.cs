bea95cb41dc9201055ce1c4b0d9d9948ff35a724
William <will2me35@gmail.com>
2021-07-27 22:43:22 +0100

    Created the Acme Account welcome screen

---
+++ welcome.cs
@@ -0,0 +1,10 @@
+using System;
+    class Welcome
+    {
+        static void Main()
+        {
+            Console.WriteLine("Welcome to Acme Accounting Systems");
+            Console.WriteLine("Rember, we\'re \"accounting\" on you");
+            Console.Read();
+        }
+    }
